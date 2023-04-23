9764afdd2f613eeced0ced363bb195590608b68b
Crl-Vlz <crlvlz0215@gmail.com>
2023-04-22 21:28:01 -0600

    Finished on some tcp bugs

--- client.c
+++ client.c
@@ -6,50 +6,181 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main() {
-    struct sockaddr_in address;
-    int sock = 0, valread;
+#define BUFFER_SIZE 1024
+#define TCP_PORT 5000
+#define UDP_PORT 5001
+#define IP_ADDR "172.18.2.2"
+
+char *parse_player(char p){
+    int player = atoi(&p);
+    switch(player){
+        case 1:
+            return "You are Player A\n";
+            break;
+        case 2:
+            return "You are Player B\n";
+            break;
+        default:
+            return "Could not define player\n";
+            break;
+    }
+}
+
+char *parse_result(char r)
+{
+    int code = atoi(&r);
+    switch (code)
+    {
+    case 1:
+        return "Player A wins!\n";
+    case 2:
+        return "Player B wins!\n";
+    case 3:
+        return "Its a tie!\n";
+    case 0:
+    default:
+        return "Communication error\n";
+    }
+}
+
+void handle_tcp_message()
+{
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-     
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-   
-    memset(&serv_addr, '0', sizeof(serv_addr));
-   
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "172.18.2.2", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation error\n");
+        return;
     }
 
-    while(1){
-        printf("Enter your choice (rock[1], paper[2], or scissors[3]): ");
-        fgets(buffer, 1024, stdin);
-        buffer[strcspn(buffer, "\n")] = 0;
-        if (strcmp(buffer, "1") == 0 || strcmp(buffer, "2") == 0 || strcmp(buffer, "3") == 0) {
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(TCP_PORT);
+
+    if (inet_pton(AF_INET, IP_ADDR, &serv_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address\n");
+        close(sock);
+        return;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Connection failed\n");
+        close(sock);
+        return;
+    }
+
+    char message[1024];
+    while (1)
+    {
+        printf("Enter your choice (rock[1], paper[2], or scissors[3]):\n");
+        fgets(message, 1024, stdin);
+        message[strcspn(message, "\n")] = 0;
+        if (strcmp(message, "1") == 0 || strcmp(message, "2") == 0 || strcmp(message, "3") == 0)
+        {
             break;
         }
-        printf("Invalid input!\n");
+        perror("Invalid input!\n");
     }
-    send(sock, buffer, strlen(buffer), 0);
-    char response[1024] = {0};
-    valread = read(sock, response, 1024);
-    if (valread < 0){
-        perror("Response error");
-        return -1;
+
+    send(sock, message, 1024, 0);
+    if (read(sock, message, 1024) < 0)
+    {
+        perror("Read error");
+        close(sock);
+        return;
     }
-    printf("The result is %s\n", response);
+    char p = *(message), r = *(message + 1);
+    printf("%s", parse_player(p));
+    printf("%s", parse_result(r));
     close(sock);
+}
+
+void handle_udp_message(char *message)
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("Socker creation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(UDP_PORT);
+
+    if (inet_pton(AF_INET, IP_ADDR, &serv_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sendto(sock, message, 1024, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    if (recvfrom(sock, message, 1024, 0, NULL, NULL) < 0)
+    {
+        perror("Could not get UDP packet\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("%s", parse_player(message[0]));
+    printf("%s", parse_result(message[1]));
+    close(sock);
+}
+
+int main()
+{
+    struct sockaddr_in address;
+    int sock = 0, udp_sock, valread;
+    struct sockaddr_in serv_addr, udp_addr;
+    char buffer[1024] = {0};
+
+    int option = -1;
+    do
+    {
+        printf("Select the option\n TCP: 1 \n UDP: 2 \n Exit: 3\n");
+        fgets(buffer, 1024, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        option = atoi(buffer);
+        switch (option)
+        {
+        case 1:
+        {
+            handle_tcp_message();
+        }
+        break;
+        case 2:
+        {
+            // UDP code goes here
+            while (1)
+            {
+                printf("Enter your choice (rock[1], paper[2], or scissors[3]):\n");
+                fgets(buffer, 1024, stdin);
+                buffer[strcspn(buffer, "\n")] = 0;
+                if (strcmp(buffer, "1") == 0 || strcmp(buffer, "2") == 0 || strcmp(buffer, "3") == 0)
+                {
+                    break;
+                }
+                perror("Invalid input!\n");
+            }
+            handle_udp_message(buffer);
+        }
+        break;
+        case 3:
+        {
+            // Exit program
+            printf("Goodbye\n");
+        }
+        break;
+        default:
+        {
+            perror("Invalid input\n");
+        }
+        break;
+        }
+    } while (option != 3);
     return 0;
 }
--- server.c
+++ server.c
@@ -6,9 +6,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <time.h>
 
 #define MAX_CLIENTS 10
 #define PORT 5000
+#define UDP_PORT 5001
+#define BUFFER_SIZE 1024
 
 int play(int a, int b)
 {
@@ -24,7 +27,7 @@ int play(int a, int b)
 		1 = Player A
 		2 = Player B
 		3 = Tie
-		-1 = Error
+		0 = Error
 
 	*/
 
@@ -44,7 +47,7 @@ int play(int a, int b)
 			c = 1;
 			break;
 		default:
-			c = -1;
+			c = 0;
 			break;
 		}
 	}
@@ -63,7 +66,7 @@ int play(int a, int b)
 			c = 2;
 			break;
 		default:
-			c = -1;
+			c = 0;
 			break;
 		}
 	}
@@ -82,13 +85,13 @@ int play(int a, int b)
 			c = 3;
 			break;
 		default:
-			c = -1;
+			c = 0;
 			break;
 		}
 	}
 	break;
 	default:
-		c = -1;
+		c = 0;
 		break;
 	}
 
@@ -117,80 +120,121 @@ int playGame(int p1_fd, int p2_fd)
 	char c[10];
 	sprintf(c, "%d", res);
 	printf("Result %s\n", c);
+	sprintf(c, "1%d", res);
+	printf("Player A gets %s\n", c);
 	send(p1_fd, c, strlen(c), 0);
+	sprintf(c, "2%d", res);
+	printf("Player B gets %s\n", c);
 	send(p2_fd, c, strlen(c), 0);
 	close(p1_fd);
-	close(p2_fd);
+	close(p1_fd);
 	return 0;
+}
+
+int randomValue(int lower, int upper){
+	return (rand() % (upper - lower + 1)) + lower;
 }
 
 int main(int argc, char const *argv[])
 {
-	int server_fd, client_fds[MAX_CLIENTS], num_clients = 0;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
+	srand(time(NULL));
+	int tcp_socket, udp_socket, client_fds[MAX_CLIENTS], num_clients = 0, client_len;
+	struct sockaddr_in tcp_address, udp_address, client_address;
+	char buffer[1024];
 
 	// Create the server socket
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	if ((tcp_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
-		perror("socket failed");
+		perror("failed to make tcp socket");
 		exit(EXIT_FAILURE);
 	}
 
-	// Bind the socket to the specified port
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-	{
-		perror("bind failed");
+	// Create UDP server socket
+	if ((udp_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		perror("failed to make UDP socket");
 		exit(EXIT_FAILURE);
 	}
 
-	// Listen for incoming connections
-	if (listen(server_fd, MAX_CLIENTS) < 0)
+	// Initialize TCP socket
+	memset(&tcp_address, 0, sizeof(tcp_address));
+	tcp_address.sin_family = AF_INET;
+	tcp_address.sin_addr.s_addr = INADDR_ANY;
+	tcp_address.sin_port = htons(PORT);
+
+	// Initialize UDP socket
+	memset(&udp_address, 0, sizeof(udp_address));
+	udp_address.sin_family = AF_INET;
+	udp_address.sin_addr.s_addr = INADDR_ANY;
+	udp_address.sin_port = htons(UDP_PORT);
+
+	// Bind TCP socket
+	if (bind(tcp_socket, (struct sockaddr *)&tcp_address, sizeof(tcp_address)) < 0)
+	{
+		perror("TCP bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	// Bind UDP socket
+	if (bind(udp_socket, (struct sockaddr *)&udp_address, sizeof(udp_address)) < 0) {
+		perror("UDP bind failure");
+		exit(EXIT_FAILURE);
+	}
+
+	// Listen for incoming connections on TCP socket
+	if (listen(tcp_socket, MAX_CLIENTS) < 0)
 	{
 		perror("listen failed");
 		exit(EXIT_FAILURE);
 	}
 
+	fd_set readfds;
+	int max_fd = (tcp_socket > udp_socket) ? tcp_socket : udp_socket, activity;
+
+	// Clear file descriptor
+	FD_ZERO(&readfds);
+
+	// Add TCP and UDP ports to file descriptors
+	FD_SET(tcp_socket, &readfds);
+	FD_SET(udp_socket, &readfds);
+
 	// Accept incoming connections
 	while (1)
 	{
-		// Wait for a new connection
-		int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-		if (new_socket < 0)
-		{
-			perror("accept failed");
+
+		if((activity = select(max_fd + 1, &readfds, NULL, NULL, NULL)) < 0 && (errno != EINTR)){
+			perror("Failed to select sockets");
 			exit(EXIT_FAILURE);
 		}
 
-		// Add the new client to the list
-		client_fds[num_clients++] = new_socket;
-
-		// If there are two clients waiting, pair them up and play the game
-		if (num_clients == 2)
-		{
-			int player1_fd = client_fds[0];
-			int player2_fd = client_fds[1];
-
-			// Play the game
-			if (playGame(player1_fd, player2_fd) < 0)
-				return -1;
-
-			// Remove the players from the list
-			for (int i = 0; i < num_clients; i++)
-			{
-				if (client_fds[i] == player1_fd || client_fds[i] == player2_fd)
-				{
-					for (int j = i; j < num_clients - 1; j++)
-					{
-						client_fds[j] = client_fds[j + 1];
-					}
-				}
+		if(FD_ISSET(tcp_socket, &readfds)){
+			int p1_fd, p2_fd;
+			if((p1_fd = accept(tcp_socket, (struct sockaddr *)&client_address, (socklen_t *)&client_len)) < 0){
+				perror("Failure connecting to client 1");
+				exit(EXIT_FAILURE);
 			}
-			num_clients -= 2;
+			if((p2_fd = accept(tcp_socket, (struct sockaddr *)&client_address, (socklen_t *)&client_len)) < 0){
+				perror("Failure connecting to client 2");
+				exit(EXIT_FAILURE);
+			}
+			if (playGame(p1_fd, p2_fd) < 0){
+				perror("Error while playing game");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (FD_ISSET(udp_socket, &readfds)){
+			printf("Activity from UDP socket");
+			if (recvfrom(udp_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_address, (struct socklen_t *)&client_len) < 0){
+				perror("Error receiving UDP packet");
+				exit(EXIT_FAILURE);
+			}
+			printf("Recieved %s from UDP packet", buffer);
+			int res = play(atoi(buffer), randomValue(1, 3));
+			snprintf(buffer, BUFFER_SIZE, "%d", res);
+			if (sendto(udp_socket, buffer, strlen(buffer), 0, (struct sockaddr *)&client_address, client_len) < 0){
+				perror("Error sending UDP packet");
+				exit(EXIT_FAILURE);
+			}
 		}
 	}
 	return 0;
